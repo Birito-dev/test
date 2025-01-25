4981a051a67cb8687ba2934eea132248536ff0ad
unknown <biritinholindo@gmail.com>
2025-01-25 11:47:17 -0300

    first commit

---
+++ desafio1guanabara.py
@@ -0,0 +1,13 @@
+nome = input('Qual é o seu nome? ') 
+
+print ('Olá',nome, 'dou-lhe boas vindas ao meu programa!') 
+
+Dia = 2 
+Mês = 'Maio'
+Ano = 2009
+
+print ('O' ,nome, 'nasceu no dia', Dia, 'de', Mês, 'de', Ano, 'correto?')
+
+input ('sim')
+
+ 
